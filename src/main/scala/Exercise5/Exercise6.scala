4ac77964c63092fa5b17617f0e2e727cb37f4f09
8kta <alonsog.gascon@gmail.com>
2022-04-25 15:22:03 -0400

    reverse with while

---
+++ src/main/scala/Exercise5/Exercise6.scala
@@ -0,0 +1,8 @@
+package Exercise5
+
+object Exercise6 extends App {
+  var num = 1234567890
+  val reversed = new ReverseWithWhile(num).reverseWithWhile()
+
+  println(reversed)
+}
---
+++ src/main/scala/Exercise6.scala
@@ -0,0 +1,3 @@
+class Exercise6 {
+
+}
