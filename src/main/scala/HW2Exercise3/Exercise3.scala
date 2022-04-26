69caa8befb690734d1c0dbd04bb1261cafbfa32b
Alonso García <alonsog.gascon@gmail.com>
2022-04-26 09:59:46 -0400

    exercise 3 scala count occurrences

--- src/main/scala/HW2Exercise3/Exercise3.scala
+++ src/main/scala/HW2Exercise3/Exercise3.scala
@@ -3,6 +3,7 @@ package HW2Exercise3
 object Exercise3 extends App {
 
   println(Functions.countOccurrences(List(1, 1, 1, 2, 2, 3, 6, 4, 4, 6, 1, 6, 2)))
+
   println(Functions.countOccurrences(List("Red", "Green", "White", "Black", "Red", "Green", "Black")))
 
 }
