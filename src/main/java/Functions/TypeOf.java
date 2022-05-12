7f55cfbea7cd53cfbf8833dcfebc2076dc87f121
8kta <alonsog.gascon@gmail.com>
2022-05-11 20:55:48 -0400

    First exampleof UDF

---
+++ src/main/java/Functions/TypeOf.java
@@ -0,0 +1,42 @@
+package Functions;
+
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.WritableStringObjectInspector;
+import org.apache.hadoop.io.Text;
+
+
+public class TypeOf extends GenericUDF {
+    private final Text output = new Text();
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
+        checkArgsSize(objectInspectors, 1, 1);
+        checkArgPrimitive(objectInspectors, 0);
+        WritableStringObjectInspector outputOI = PrimitiveObjectInspectorFactory.writableStringObjectInspector;
+        return outputOI;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
+        Object obj;
+        if ((obj = deferredObjects[0].get()) == null) {
+            String res = "Type: NULL";
+            output.set(res);
+        } else {
+            String res = "Type: " + obj.getClass().getName();
+            output.set(res);
+        }
+        return output;
+    }
+
+    @Override
+    public String getDisplayString(String[] strings) {
+        return getStandardDisplayString("TYPEOF", strings, ",");
+    }
+}
